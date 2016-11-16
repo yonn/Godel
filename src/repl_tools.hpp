d3d6a188173696b5705cde76b794834886e1e048
Yogev Onn <yogevonn@gmail.com>
2016-11-16 12:12:04 +0200

    Added light documentation

--- src/repl_tools.hpp
+++ src/repl_tools.hpp
@@ -9,6 +9,12 @@
 
 namespace godel {
 
+	/**
+	 * read_line returns a std::string, with the line that 
+	 * was read, without the new-line character ('\n')
+	 *
+	 * prompt: string literal of the prompt to be printed out 
+	 */
 	std::string read_line(const char* prompt);
 
 }
